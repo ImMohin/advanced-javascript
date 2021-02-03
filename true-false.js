68a33f813b0e5ba1c588c0e7b6ee32447683377f
ImMohin <mohinuddin252@gmail.com>
2021-02-03 11:22:28 +0600

    true vs  false checking

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// False condition 
+// undefined
+// null
+// NaN
+// ""
+// 0
+// false 
+
+// True Condition
+// '0' , ' ' , true , [] , {} , 1 
+let age = 1;
+
+if(age){
+    console.log("condition is True");
+}
+else{
+    console.log("condition is False");
+}
