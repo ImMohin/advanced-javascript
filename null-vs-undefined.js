a97d197625685f7d3b15ab33dcd67a7107a574b4
ImMohin <mohinuddin252@gmail.com>
2021-02-03 11:47:08 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,43 @@
+// undefined value checking 
+
+// variable value not assign
+
+// let age;
+// console.log(age);
+
+
+// // function value not return 
+
+// function add(num1, num2){
+//     const summ = num1 + num2;
+//     // return 
+// }
+// let sum = add(12, 20);
+// console.log(sum);
+
+
+// Function call korar somoy parameter pass na PublicKeyCredential 
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(12);
+// console.log(result);
+
+
+// object ar property na thkle jdi amn kisu access krte jay 
+
+// const premik = {name: "mohin", phone: 1254};
+// console.log(premik.fatherName);
+
+
+// variable ar value undefined set krle 
+
+// let fun = undefined;
+// console.log(fun);
+
+
+
+
+// Null number 
+//  null means empty value set variable 
